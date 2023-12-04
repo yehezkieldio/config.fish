eab695bf4a48dc88b6882fd1f7a8f0fe3ac3a622
elizielx <yehezkieldio35@gmail.com>
2023-12-05 06:51:13 +0800

    chore: add pnpm completion

--- config.fish
+++ config.fish
@@ -60,3 +60,7 @@ if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
+
+# tabtab source for packages
+# uninstall by removing these lines
+[ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
